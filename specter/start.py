4703afb6734a86beb9d2341d72a765ba3d176e98
pigcake999yt <999pigcake@gmail.com>
2017-04-09 20:00:12 +0100

    Add files via upload

--- specter/start.py
+++ specter/start.py
@@ -1,20 +1,34 @@
 import sys
 import os
+if sys.argv[1] == "server":
+	PORT = input("What port is your server on?")
 
-filedir = os.getcwd()+'/package.json';
+	import webbrowser
 
-details = open(filedir, "r");
+	appname = "PhpApp"
 
-obj = details.read()
+	filename = "index.php"
 
-print(obj)
+	url = "localhost:"+PORT+"/"+appname+'/'+filename
 
-import webbrowser
+	webbrowser.open('file:///Developer/specter/open.script.html?url='+url+'&new=window&width=800&height=600&appname='+appname,new=2)
+pass
+if sys.argv[1] == "app":
+	filedir = os.getcwd()+'/package.json';
 
-appname = "MyApp"
+	details = open(filedir, "r");
 
-filename = "index.html"
+	obj = details.read()
 
-url = os.getcwd()+'/'+filename
+	print("Starting app...")
 
-webbrowser.open('file:///Developer/specter/open.script.html?url='+url+'&new=window&width=800&height=600&appname='+appname,new=2)
+	import webbrowser
+
+	appname = "My Specter App"
+
+	filename = "index.php"
+
+	url = os.getcwd()+'/'+filename
+
+	webbrowser.open('file:///Developer/specter/open.script.html?url='+url+'&new=window&width=800&height=600&appname='+appname,new=2)
+pass
